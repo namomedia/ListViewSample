96e41dc9fb28162af779e4681fc65f7d759ee79d
Joe Blubaugh <joe@namomedia.com>
2014-07-10 14:30:05 -0700

    Remove IDEA directory, update build scripts.

--- src/com/namomedia/samples/SampleApplication.java
+++ src/com/namomedia/samples/SampleApplication.java
@@ -1,6 +1,7 @@
 package com.namomedia.samples;
 
 import android.app.Application;
+import com.namomedia.android.Namo;
 import com.parse.Parse;
 import com.parse.ParseObject;
 
@@ -14,8 +15,9 @@ public class SampleApplication extends Application {
     Parse.initialize(this, "U8ipUhuguVcq8zid2nIH1OU7jvwYuBi2G2QBzUqT",
         "JYq1sRB1ZBh6AmD0UvGNVsLq5DwyIr5PnZikF1QN");
 
+    Namo.initialize(this, "app-test-id");
     // Replace "11aa69fe03877062" with your device's ID. When you initialize the SDK
     // your device's ID will be printed to the console in an I/Namo level logging message.
-    // Namo.setTestDevices(true, "11aa69fe03877062");
+    Namo.setTestDevices(true, "11aa69fe03877062");
   }
 }
